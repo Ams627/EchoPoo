54488d08468b13e127a4b7c74629b964a684850e
Adrian Sims <adriansims62781@gmail.com>
2020-01-22 22:50:49 +0100

    Add project files.

---
+++ EchoPoo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int t💩 = 10;
+        using (var stdout = Console.OpenStandardOutput())
+        {
+            new List<byte> { 0xF0, 0x9F, 0x92, 0xA9, 0xD, 0xA }.ForEach(stdout.WriteByte);
+        }
+    }
+}
+
